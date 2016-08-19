3344de80e23f044d42f3b533528d4315f97820ec
brianlee2020 <bl0064@bard.edu>
2016-08-19 15:27:38 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -5,7 +5,7 @@
  */
 
 var things = ["Music", "Art", "Dance", "Theater", "Math", "Architecture"];
-var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect", "footballers"]
+var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect", "footballer"]
 var castles = ["museums", "bridges", "cities"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
 var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
@@ -35,7 +35,7 @@ q.innerHTML =
     "\"The " + programmer +
     ", like the poet, works only slightly removed from pure thought-stuff. He builds his " +
     castles + " in the " + air + ", from " + air + ", creating by " + exertion + " of the imagination.\"" +
-    "\u2014" +  "Fred Brooks, " + programmer; "You have to fight to reach your dream. You have to sacrifice and work hard for it."
+    "\u2014" +  "Fred Brooks, " + programmer; 
 q.innerHTML += "<br><br><br>";
 
 
